6cc97eaa1cd3e2405f7d8cb2972ea207fea7ef0b
ramanathan07 <89630494+ramanathan07@users.noreply.github.com>
2023-03-31 12:31:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function menudisp(){
+    var x=document.getElementById("mob");
+    if(x.style.display === "none")
+    {
+        x.style.display="block";
+    }
+    else{
+        x.style.display="none";
+    }
+}
